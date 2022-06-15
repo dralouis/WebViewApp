225d7f003f224705f3c0a69d13b386b0492e346e
Dralouis <dralouis01@gmail.com>
2022-06-14 18:23:12 -0700

    web view first commit

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.webviewapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    private val web : WebView?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val web = findViewById<WebView>(R.id.webview)
+        web .webViewClient = WebViewClient()
+        web.loadUrl("www.intellectsoft.net")
+        val webSettings=web.settings
+        webSettings.javaScriptEnabled = true
+    }
+
+    override fun onBackPressed() {
+        if (web!!.canGoBack()){
+            web.goBack()
+        }
+        super.onBackPressed()
+    }
+}
